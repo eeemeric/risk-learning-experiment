9e3a091080a11f7baf41f531d8ae82772c699133
Melodi Inceboz <102524169+lomedi@users.noreply.github.com>
2026-01-07 11:07:13 -0500

    Update mkturk_installsettings.js

--- mkturk_installsettings.js
+++ mkturk_installsettings.js
@@ -6,7 +6,7 @@
 // MKTURK.HTML & LIVEPLOT.HTML
 // ------ Dropbox WebApp settings ------ 
 var DBX_CLIENT_ID = "mfdmydq5uf67my5"
-var DBX_REDIRECT_URI_ROOT = "https://github.com/eeemeric/risk-learning-experiment/blob/7342779a1d33e71648e6ee06109059b2412bc156/index.html"
+var DBX_REDIRECT_URI_ROOT = "https://lomedi.github.io/risk-learning-experiment/Self-Control-Experiment/"
 
 // ------ Subject settings ------ 
 var subjectlist = ["SelfControlTask"];
@@ -19,6 +19,7 @@ var SOUND_FILEPREFIX = "/mkturkfolders/sounds/au"
 // ------ Misc. -----------------------
 var ndatafiles2read=5; // todo: change to trials. and use as upper bound (stop reading once you hit the first discrepancy). maybe this is effectively synonymous with mintrials
 var num_preload_images=0; // how long can you/the NHP bother waiting at each imageload? 400 images ~ 30 seconds. Recommended to keep = 0 with good internet connection and automator on
+
 
 
 

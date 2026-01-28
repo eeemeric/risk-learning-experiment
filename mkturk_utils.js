8aa82365e3b2e9b4303c6b80cb5b8d81d971a7ad
erik emeric <erik.emeric@gmail.com>
2026-01-28 14:00:51 -0500

    Increase wait time before next reward from 400ms to 500ms

--- mkturk_utils.js
+++ mkturk_utils.js
@@ -407,8 +407,8 @@ async function showOutcomeAndDeliverReward(rewardCount, position, loadedImages, 
             logDebug(`No device connected`);
         }
         
-        logDebug(`Waiting 400ms before next reward...`);
-        await new Promise(resolve => setTimeout(resolve, 400));
+        logDebug(`Waiting 500ms before next reward...`);
+        await new Promise(resolve => setTimeout(resolve, 500));
         logDebug(`Inter-reward wait complete`);
     }
     
